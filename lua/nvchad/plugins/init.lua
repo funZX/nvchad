8e29a82aee6e5c23623a394ecc4df2418545b286
Adrian SIMINCIUC <asiminciuc@yahoo.com>
2026-04-22 18:19:23 +0200

    fix

--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -163,7 +163,7 @@ return {
       return require "nvchad.configs.treesitter"
     end,
     config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+      require("nvim-treesitter.config").setup(opts)
     end,
   },
 
