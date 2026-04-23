68491448d4b8803e4c353126842e27cd6049af71
Adrian SIMINCIUC <asiminciuc@yahoo.com>
2026-04-24 00:11:04 +0200

    lsp update

--- lua/nvchad/configs/lspconfig.lua
+++ lua/nvchad/configs/lspconfig.lua
@@ -86,14 +86,14 @@ M.defaults = function()
       },
   }
 
-  vim.lsp.config('tsserver', {
+  vim.lsp.config.typescript = {
     cmd = {'typescript-language-server', '--stdio'},
     filetypes = { 'typescript' },
     root_dir = vim.fs.root(0, {'package.json', '.git'}),
   })
   -- Use new vim.lsp.config API for Neovim 0.11+
-  vim.lsp.config("*", { capabilities = M.capabilities, on_init = M.on_init })
-  vim.lsp.enable("clangd", "tsserver", "robot", "ruff", "copilot_ls")
+  vim.lsp.config("*", { capabilities = M.capabilities, on_init = M.on_init, on_attach = M.on_attach })
+  vim.lsp.enable("clangd", "typescript", "robot", "ruff", "copilot_ls")
 
   local dap, dapui = require("dap"), require("dapui")
 
