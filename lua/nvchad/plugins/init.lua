bae56e676f2ea738f952a54a4ddad48002e7ee95
Adrian SIMINCIUC <asiminciuc@yahoo.com>
2026-04-24 15:30:09 +0200

    ident

--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -35,15 +35,10 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     event = "User FilePost",
     opts = {
-      indent = { char = "¦", highlight = "IblChar" },
-      scope = { char = "¦", highlight = "IblScopeChar" },
     },
     config = function(_, opts)
       dofile(vim.g.base46_cache .. "blankline")
-
       require("ibl").setup(opts)
-      local hooks = require "ibl.hooks"
-      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
     end,
   },
   {
