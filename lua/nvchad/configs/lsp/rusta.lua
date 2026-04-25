c7d054427d0187204839d289e71f82d9597a3da9
Adrian SIMINCIUC <asiminciuc@yahoo.com>
2026-04-25 12:07:36 +0200

    update

--- lua/nvchad/configs/lsp/rusta.lua
+++ lua/nvchad/configs/lsp/rusta.lua
@@ -7,7 +7,7 @@
 ---
 --- See [docs](https://rust-analyzer.github.io/book/configuration.html) for extra settings. The settings can be used like this:
 --- ```lua
---- vim.lsp.config('rust_analyzer', {
+--- vim.lsp.config('rusta', {
 ---   settings = {
 ---     ['rust-analyzer'] = {
 ---       diagnostics = {
@@ -22,7 +22,7 @@
 --- https://github.com/rust-lang/rust-analyzer/blob/eb5da56d839ae0a9e9f50774fa3eb78eb0964550/docs/dev/lsp-extensions.md?plain=1#L26.
 
 local function reload_workspace(bufnr)
-  local clients = vim.lsp.get_clients { bufnr = bufnr, name = 'rust_analyzer' }
+  local clients = vim.lsp.get_clients { bufnr = bufnr, name = 'rusta' }
   for _, client in ipairs(clients) do
     vim.notify 'Reloading Cargo Workspace'
     ---@diagnostic disable-next-line:param-type-mismatch
@@ -36,11 +36,11 @@ local function reload_workspace(bufnr)
 end
 
 local function user_sysroot_src()
-  return vim.tbl_get(vim.lsp.config['rust_analyzer'], 'settings', 'rust-analyzer', 'cargo', 'sysrootSrc')
+  return vim.tbl_get(vim.lsp.config['rusta'], 'settings', 'rust-analyzer', 'cargo', 'sysrootSrc')
 end
 
 local function default_sysroot_src()
-  local sysroot = vim.tbl_get(vim.lsp.config['rust_analyzer'], 'settings', 'rust-analyzer', 'cargo', 'sysroot')
+  local sysroot = vim.tbl_get(vim.lsp.config['rusta'], 'settings', 'rust-analyzer', 'cargo', 'sysroot')
   if not sysroot then
     local rustc = os.getenv 'RUSTC' or 'rustc'
     local result = vim.system({ rustc, '--print', 'sysroot' }, { text = true }):wait()
@@ -75,7 +75,7 @@ local function is_library(fname)
 
   for _, item in ipairs { toolchains, registry, git_registry, sysroot_src } do
     if item and vim.fs.relpath(item, fname) then
-      local clients = vim.lsp.get_clients { name = 'rust_analyzer' }
+      local clients = vim.lsp.get_clients { name = 'rusta' }
       return #clients > 0 and clients[#clients].config.root_dir or nil
     end
   end
@@ -83,7 +83,7 @@ end
 
 ---@type vim.lsp.Config
 return {
-  name = "rust",
+  name = "rusta",
   cmd = { 'rust-analyzer' },
   filetypes = { 'rust' },
   root_dir = function(bufnr, on_dir)
@@ -127,7 +127,7 @@ return {
         on_dir(cargo_workspace_root or cargo_crate_dir)
       else
         vim.schedule(function()
-          vim.notify(('[rust_analyzer] cmd failed with code %d: %s\n%s'):format(output.code, cmd, output.stderr))
+          vim.notify(('[rusta] cmd failed with code %d: %s\n%s'):format(output.code, cmd, output.stderr))
         end)
       end
     end)
@@ -144,7 +144,7 @@ return {
       },
     },
   },
-  ---@type lspconfig.settings.rust_analyzer
+  ---@type lspconfig.settings.rusta
   settings = {
     ['rust-analyzer'] = {
       lens = {
