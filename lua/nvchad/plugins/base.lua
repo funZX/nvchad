58ba5bfe26fd5dca098d2026cf8b984beadfb895
Adrian SIMINCIUC <asiminciuc@yahoo.com>
2026-04-27 13:05:38 +0200

    +which-key

--- lua/nvchad/plugins/base.lua
+++ lua/nvchad/plugins/base.lua
@@ -50,6 +50,15 @@ return {
     end,
   },
 
+  {
+    "folke/which-key.nvim",
+    keys = { "<leader>", "<c-w>", '"', "'", "`", "c", "v", "g" },
+    cmd = "WhichKey",
+    opts = function()
+      dofile(vim.g.base46_cache .. "whichkey")
+      return {}
+    end,
+  },
 
   {
     "nvim-treesitter/nvim-treesitter",
