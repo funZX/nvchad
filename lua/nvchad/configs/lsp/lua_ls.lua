9dca400d75e754f5487f2f77df591db3f59310eb
Adrian SIMINCIUC <asiminciuc@yahoo.com>
2026-04-28 21:06:25 +0200

    lua_ls + luajit

--- lua/nvchad/configs/lsp/lua_ls.lua
+++ lua/nvchad/configs/lsp/lua_ls.lua
@@ -92,6 +92,15 @@ return {
     Lua = {
       codeLens = { enable = true },
       hint = { enable = true, semicolon = 'Disable' },
+      runtime = { version = "LuaJIT" },
+      workspace = {
+        library = {
+          vim.fn.expand "$VIMRUNTIME/lua",
+          vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
+          vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
+          "${3rd}/luv/library",
+        },
+      },
     },
   },
 }
