aefc133f126c0f1717e9adc9eaa691b1e520002e
Adrian SIMINCIUC <asiminciuc@yahoo.com>
2026-04-28 23:53:07 +0200

    cleanup

--- lua/nvchad/configs/lsp/lua_ls.lua
+++ lua/nvchad/configs/lsp/lua_ls.lua
@@ -96,8 +96,6 @@ return {
       workspace = {
         library = {
           vim.fn.expand "$VIMRUNTIME/lua",
-          vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
-          "${3rd}/luv/library",
         },
       },
     },
