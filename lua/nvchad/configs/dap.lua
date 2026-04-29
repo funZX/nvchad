1aa755e2a9190e318a667e140379e125bb99d16f
Adrian SIMINCIUC <asiminciuc@yahoo.com>
2026-04-29 10:56:56 +0200

    dap c++

--- lua/nvchad/configs/dap.lua
+++ lua/nvchad/configs/dap.lua
@@ -1,5 +1,12 @@
 local dap = require("dap")
 
+dap.adapters.gdb = {
+    name = 'gdb',
+    type = 'executable',
+    command = '/usr/bin/gdb', -- adjust as needed, must be absolute path
+    args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+}
+
 dap.adapters.lldb = {
     type = 'executable',
     command = '/usr/bin/lldb-dap', -- adjust as needed, must be absolute path
@@ -8,15 +15,38 @@ dap.adapters.lldb = {
 
 dap.configurations.cpp = {
     {
+        name = 'Remote',
+        type = 'gdb',
+        request = 'attach',
+        target = function()
+          return vim.fn.input('Host: ') .. ':2000'
+        end
+    },
+    {
+        name = 'Attach',
+        type = 'gdb',
+        request = 'attach',
+        pid = function()
+          local name = vim.fn.input('Process filter: ')
+          return require("dap.utils").pick_process({filter = name})
+        end
+    },
+    {
         name = 'Launch',
-        type = 'lldb',
+        type = 'gdb',
+        --type = 'lldb',
         request = 'launch',
         program = function()
             return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
         end,
+        args = function()
+            local args_str = vim.fn.input({
+                prompt = 'Arguments: ',
+            })
+            return vim.split(args_str, ' :')
+        end,
         cwd = '${workspaceFolder}',
-        stopOnEntry = false,
-        args = {},
+        stopAtBeginningOfMainSubprogram = true,
     },
 }
 
