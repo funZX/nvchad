905d5de927d6e7b500b8a703dced0b77ada3e057
Adrian SIMINCIUC <asiminciuc@yahoo.com>
2026-05-05 18:12:04 +0200

    +wiki

--- lua/nvchad/plugins/editor.lua
+++ lua/nvchad/plugins/editor.lua
@@ -1,2 +1,17 @@
 return {
+  {
+    "echaya/neowiki.nvim",
+    opts = {
+      wiki_dirs = {
+        -- neowiki.nvim supports both absolute and tilde-expanded paths
+        { name = "Work",     path = "~/work/wiki" },
+        { name = "Personal", path = "personal/wiki" },
+      },
+    },
+    keys = {
+      { "<leader>ww", "<cmd>lua require('neowiki').open_wiki()<cr>",          desc = "Open Wiki" },
+      { "<leader>wf", "<cmd>lua require('neowiki').open_wiki_floating()<cr>", desc = "Open Wiki in Floating Window" },
+      { "<leader>wt", "<cmd>lua require('neowiki').open_wiki_new_tab()<cr>",  desc = "Open Wiki in Tab" },
+    },
+  }
 }
